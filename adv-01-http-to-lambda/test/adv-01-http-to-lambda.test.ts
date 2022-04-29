e4cf62d152d9978738f2f2f422ec2b5c7d163fc7
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2022-04-30 00:04:45 +0500

    Init adv-01-http-to-lambda
    Init adv-01-http-to-lambda

---
+++ adv-01-http-to-lambda/test/adv-01-http-to-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Adv01HttpToLambda from '../lib/adv-01-http-to-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/adv-01-http-to-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Adv01HttpToLambda.Adv01HttpToLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
