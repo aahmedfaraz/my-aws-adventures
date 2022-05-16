b2f1fa2aeeb49d5e755a5a59f11adca9841722bb
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2022-05-17 01:03:02 +0500

    Initialize adv-02
    Initialize adv-02

---
+++ adv-02-python-lambda-7zip-dp/test/adv-02-python-lambda-7zip-dp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Adv02PythonLambda7ZipDp from '../lib/adv-02-python-lambda-7zip-dp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/adv-02-python-lambda-7zip-dp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Adv02PythonLambda7ZipDp.Adv02PythonLambda7ZipDpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
