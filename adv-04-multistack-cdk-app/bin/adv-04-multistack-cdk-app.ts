89f4726d3de8ab0089f2ffa4ad4b809cda687014
Ahmed Faraz <ahmedfaraz.contact@gmail.com>
2022-12-07 00:42:10 +0500

    completed multistack app

---
+++ adv-04-multistack-cdk-app/bin/adv-04-multistack-cdk-app.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { Stack1 } from '../lib/stack1';
+import { Stack2 } from '../lib/stack2';
+
+const app = new cdk.App();
+new Stack1(app, `Stack1`, {
+});
+new Stack2(app, `Stack2`, {
+});
+
+app.synth();
---
+++ adv-04-multistack-cdk-app/test/adv-04-multistack-cdk-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Adv04MultistackCdkApp from '../lib/adv-04-multistack-cdk-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/adv-04-multistack-cdk-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Adv04MultistackCdkApp.Adv04MultistackCdkAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
